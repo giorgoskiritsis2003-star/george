361a81f6e1054a1aabf07c861f4b3967764d4abf
george <giorgoskiritsis2003@gmail.com>
2026-06-25 11:17:40 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,93 @@
+import streamlit as st 
+import datetime
+import requests
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #FF0000;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("Game Roulette 🎡")
+st.write("Διαλέξτε παιχνίδι για τον Γιώργο. Πρέπει να το κάνω ή plat ή να έχω κάνει όλα τα side missions και το story.")
+
+user_name = st.text_input("Γράψτε το όνομά σας:")
+catagory = st.radio(
+    "Τι θέλετε να παίξει ο Γιώργος;",
+    ["Devil May Cry (1-5)", "Nioh (1, 2)", "Metaphor", "P5 Strikers", "Dark Souls (2, 3)", "Persona DLC", "Sekiro", "Ghost of Tsushima", "Prince of Persia", "Soul Hackers 2", "Little Nightmares 3", "Sackboy", "Nine Sols", "Blasphemous", "Final Fantasy", "Resident Evil 7 (με παρέα μόνο)", "Spider-Man(1,2)"],
+    index=None,
+)
+
+final_game = catagory
+
+
+if st.button('Submit my picks '):
+    webhook_url = "https://discord.com/api/webhooks/1519616499390746697/BwFQFOq7gcM3IcGxNhGizcBJLjJwCx-S7-0AqlaQmMEHC_tuh7a6fdtFanCW22ZZJdY4" 
+    
+    with open("votes.txt", "a", encoding="utf-8") as myfile:
+        myfile.write(f"{user_name},{final_game}\n")
+
+    to_minima_mou = f"Game Roulette!\n"
+    to_minima_mou += f"Άτομο: {user_name}\n"
+    to_minima_mou += f"Παιχνίδι: {final_game}\n"
+        
+    data = {"content": to_minima_mou}
+    requests.post(webhook_url, json=data)
+
+    st.success("Τα δεδομένα στάλθηκαν επιτυχώς!")
+
+
+st.write("---")
+st.header(" Admin")
+
+admin_password = st.text_input("Κωδικός:", type="password")
+
+if admin_password == "2905":
+    if st.button("Λήξη Ψηφοφορίας & Αποτελέσματα "):
+        
+        webhook_url = "https://discord.com/api/webhooks/1519616499390746697/BwFQFOq7gcM3IcGxNhGizcBJLjJwCx-S7-0AqlaQmMEHC_tuh7a6fdtFanCW22ZZJdY4" 
+        
+        try:
+            
+            with open("votes.txt", "r", encoding="utf-8") as file:
+                lines = file.readlines()
+                
+           
+            votes_count = {}
+            for line in lines:
+                parts = line.strip().split(",")
+                if len(parts) == 2:
+                    game = parts[1]
+                    if game in votes_count:
+                        votes_count[game] += 1
+                    else:
+                        votes_count[game] = 1
+            
+            
+            if votes_count:
+                results_msg = " Η ΨΗΦΟΦΟΡΙΑ ΕΚΛΕΙΣΕ! ΟΡΙΣΤΕ ΤΑ ΑΠΟΤΕΛΕΣΜΑΤΑ:** \n\n"
+                
+                for game, count in votes_count.items():
+                    bar = "🟩" * count  
+                    results_msg += f"**{game}**: {bar} ({count})\n"
+                
+                
+                winner = max(votes_count, key=votes_count.get)
+                results_msg += f"\n Ο ΝΙΚΗΤΗΣ ΕΙΝΑΙ: {winner}!"
+                
+                
+                data = {"content": results_msg}
+                requests.post(webhook_url, json=data)
+                
+                st.success("Τα αποτελέσματα στάλθηκαν στο Discord με επιτυχία!")
+                st.balloons()
+            else:
+                st.warning("Δεν υπάρχουν ψήφοι ακόμα στο αρχείο!")
+                
+        except FileNotFoundError:
+            st.error("Το αρχείο των ψήφων δεν υπάρχει. Πρέπει να ψηφίσει κάποιος πρώτα!")
